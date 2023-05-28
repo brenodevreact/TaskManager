e7e920b603eaa15923bb0ecf81d926f4f464f291
Your Name01~ <brenoleitepereira@gmail.com>
2023-05-28 07:48:23 -0300

    Feat: Header adicionado

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -4,40 +4,52 @@ import style from "./style.module.css"
 import { useForm, SubmitHandler } from 'react-hook-form';
 import FormCadastro from '@/components/Form/FormCadastro/FormCadastro';
 import Link from 'next/link';
+import Header from '@/components/Header/header';
 
 type Inputs = {
-    email: string,
-    name: string,
-    password: string,
-    confirmPassword: string,
-    img: string,
+  email: string,
+  name: string,
+  password: string,
+  confirmPassword: string,
+  img: string,
 };
 
 const page = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = (data, event) => {
-      event?.preventDefault()
-      console.log(data)
-    }
+  const onSubmit: SubmitHandler<Inputs> = (data, event) => {
+    event?.preventDefault()
+    console.log(data)
+  }
 
   return (
     <div className={style.mainCadastro}>
-        <FormCadastro onSubmit={handleSubmit(onSubmit)}>
+      <Header>
+        <Link href={"/"}>
+          <h2>TManager</h2>
+        </Link>
+
+        <nav>
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/"}>Home</Link>
+        </nav>
+      </Header>
+
+      <FormCadastro onSubmit={handleSubmit(onSubmit)}>
         <h2>Cadastro</h2>
-            <input type="email" {...register("email")} placeholder='Email...'/>
-            <input type="text" {...register("name")} placeholder='Nome...'/>
-            <input type="password" {...register("password")} placeholder='Senha...'/>
-            <input type="password" {...register("confirmPassword")} placeholder='Confirme sua senha...'/>
-            <input type="text" {...register("img")} placeholder='Imagem de perfil...'/>
-            <button type="submit">Enviar</button>
+        <input type="email" {...register("email")} placeholder='Email...' />
+        <input type="text" {...register("name")} placeholder='Nome...' />
+        <input type="password" {...register("password")} placeholder='Senha...' />
+        <input type="password" {...register("confirmPassword")} placeholder='Confirme sua senha...' />
+        <input type="text" {...register("img")} placeholder='Imagem de perfil...' />
+        <button type="submit">Enviar</button>
 
-            <Link href={"/login"}>Já possui cadastro?</Link>
+        <Link href={"/login"}>Já possui cadastro?</Link>
 
-        </FormCadastro>
-        <div>
-          <h2>Espaço para lottie</h2>
-        </div>
+      </FormCadastro>
+      <div>
+        <h2>Espaço para lottie</h2>
+      </div>
     </div>
   )
 }
